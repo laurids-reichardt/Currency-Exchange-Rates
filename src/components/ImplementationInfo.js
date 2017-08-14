11d0eb82adb5535b24adc7d894bdc7e114e49c30
Unknown <lau.reichardt@gmail.com>
2017-08-14 14:22:48 +0200

    Update implementation info

--- src/components/ImplementationInfo.js
+++ src/components/ImplementationInfo.js
@@ -4,19 +4,18 @@ const ImplementationInfo = () =>
   <div style={{ hyphens: 'none' }}>
     <p>
       Data is sourced from <a href="http://fixer.io/">fixer.io</a> and represents the latest foreign exchange rates
-      published by the European Central Bank.
+      published by the European Central Bank. There are no guarantees on the correctness of the data displayed on this
+      website nor are there any uptime guarantees. Please use for educational purposes only.
     </p>
     <p>
       Libraries and frameworks used include <a href="https://facebook.github.io/react/">React</a> and{' '}
       <a href="https://react.semantic-ui.com">Semantic UI</a> for visualization,{' '}
       <a href="https://github.com/mzabriskie/axios">Axios</a> and <a href="https://momentjs.com/">Moment.js</a> to fetch
       the currency rate data and <a href="http://formidable.com/open-source/victory/">Victory</a> to display the chart.
+      Check out the source code on <a href="https://github.com/laurids-reichardt/Currency-Exchange-Rates">GitHub</a> for
+      further details.
     </p>
-    <p>
-      There are no guarantees on the correctness of the data displayed on this website. Please use for educational
-      purposes only. Check out the source code on{' '}
-      <a href="https://laurids-reichardt.github.io/exchange-rates/">GitHub</a> for further details.
-    </p>
+    <p />
   </div>;
 
 export default ImplementationInfo;
--- src/components/InfoText.js
+++ src/components/InfoText.js
@@ -4,7 +4,7 @@ const InfoText = () =>
   <div style={{ hyphens: 'none' }}>
     <p>
       Conveniently look up current foreign exchange rates and convert from one to another currency. The chart displays
-      the exchange rate development over the last twelve weeks. See below for further details.
+      the exchange rate development over the last twelve weeks. See below for further details on the implementation.
     </p>
   </div>;
 
